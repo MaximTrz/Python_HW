e8571a4d33c8f2180ac7e37a1d0437520504b7b9
Maxim <maxim.trz@gmail.com>
2022-06-03 13:19:36 +0500

    hw_2

---
+++ HomeWork_week_2.py
@@ -0,0 +1,40 @@
+cars = {
+    "Honda": {
+        "Civic":{
+            "Value": 1.8,
+            "Body_type":  "Sedan",
+            "Power": 140,                        
+        },
+        "CR-V":{
+            "Value": 2.0,
+            "Body_type":  "Station wagon",
+            "Power": 150,     
+        } 
+    },
+    "Toyota":{
+        "Corolla":{
+            "Value": 1.6,
+            "Body_type":  "Sedan",
+            "Power": 120,   
+        },
+        "RAV-4":{
+            "Value": 2.0,
+            "Body_type":  "Station wagon",
+            "Power": 152,     
+        } 
+    }
+}
+
+cars_list = [
+    ["Honda Civic", 1.8, "Sedan", 140],
+    ["Honda CR-V", 2.0, "Station wagon", 150],
+    ["Toyota RAV-4", 2.0,  "Station wagon", 152],
+    ["Toyota Corolla", 1.8, "Sedan", 120]
+]
+
+cars_3 = {
+    ("Honda Civic", 1.8, "Sedan", 140),
+    ("Honda CR-V", 2.0, "Station wagon", 150),
+    ("Toyota RAV-4", 2.0,  "Station wagon", 152),
+    ("Toyota Corolla", 1.8, "Sedan", 120)
+}
