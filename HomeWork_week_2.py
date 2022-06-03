ef8f8c38d49120009267541b8d40d54427b6e7de
Maxim <maxim.trz@gmail.com>
2022-06-03 14:22:30 +0500

    search_dict

--- HomeWork_week_2.py
+++ HomeWork_week_2.py
@@ -1,4 +1,4 @@
-cars = {
+cars_dict = {
     "Honda": {
         "Civic":{
             "Value": 1.8,
@@ -32,9 +32,27 @@ cars_list = [
     ["Toyota Corolla", 1.8, "Sedan", 120]
 ]
 
-cars_3 = {
+cars_set = {
     ("Honda Civic", 1.8, "Sedan", 140),
     ("Honda CR-V", 2.0, "Station wagon", 150),
     ("Toyota RAV-4", 2.0,  "Station wagon", 152),
     ("Toyota Corolla", 1.8, "Sedan", 120)
 }
+
+def searchByDict(mark, model):
+    markPosition = -1
+    modelPostion = -1
+    res = []
+    
+    for key_i in cars_dict:
+        key = str(key_i)            
+        if (key==mark):                  
+            res.append(key)
+        for key_j in cars_dict[key_i]:
+            key = str(key_j)
+            if (key == model):
+                res.append(key)
+                res.append(cars_dict[key_i][key_j])
+    return(res)
+
+print(searchByDict("Honda", "Civic"))
